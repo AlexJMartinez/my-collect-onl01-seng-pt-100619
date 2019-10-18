649bc0ef33f4d7320c18a2789e75db159a9a8ed9
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-18 02:33:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,8 +5,8 @@ def my_collect(collection)
   new_collection = []
   while i < collection.length
     new_collection << yield(collection[i])
-    i.split(" ") += 1
+    i += 1
   end
-  new_collection
+  new_collection.split(" ").first
   binding.pry
 end
